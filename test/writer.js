f719921cf276c94bf2925e99fc03e81d7befca5d
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2014-10-08 14:14:09 -0500

    Fixed a bug that caused ari client to not load.

--- test/writer.js
+++ test/writer.js
@@ -107,10 +107,10 @@ var getMockClient = function() {
  */
 var getMockConfig = function() {
   var ariConfig = {
-    url: '',
-    username: '',
-    password: '',
-    applicationName: ''
+    url: 'http://localhost:8088',
+    username: 'asterisk',
+    password: 'asterisk',
+    applicationName: 'test'
   };
 
   return {
@@ -257,9 +257,13 @@ describe('mailbox', function() {
     mockery.enable(mockeryOpts);
 
     var clientMock = {
-      getClient: function(url, username, password, appName) {
+      getClient: function(config, appName) {
         var deferred = Q.defer();
-        deferred.resolve(getMockClient());
+
+        if (config.url && config.username &&
+            config.password && appName) {
+          deferred.resolve(getMockClient());
+        }
 
         return deferred.promise;
       }
