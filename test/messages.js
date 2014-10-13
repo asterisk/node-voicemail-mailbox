aa93689d666ed30fbb147d6f19bc5d7410b91c94
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2014-10-12 21:43:41 -0500

    Mailbox Reader module for Asterisk Voicemail.

---
+++ test/messages.js
@@ -0,0 +1,26 @@
+/**
+ *  Messages Helper specific unit tests.
+ *
+ *  @module messages-test 
+ *  @copyright 2014, Digium, Inc.
+ *  @license Apache License, Version 2.0
+ *  @author Samuel Fortier-Galarneau <sgalarneau@digium.com>
+ */
+
+'use strict';
+
+/*global describe:false*/
+/*global beforeEach:false*/
+/*global it:false*/
+
+var assert = require('assert');
+
+describe('messages helper', function() {
+
+  it('it should have all expected methods', function(done) {
+    assert(1 === 2);
+  
+    done();
+  });
+
+});
